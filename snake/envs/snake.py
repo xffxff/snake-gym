694129a39e623feb16b9917b41335ad42888ad2c
xff <sailbafan@gmail.com>
2019-01-11 14:38:27 +0800

    ^_^

--- snake/envs/snake.py
+++ snake/envs/snake.py
@@ -6,6 +6,8 @@ import numpy as np
 from collections import deque
 from gym.envs.classic_control import rendering
 
+from board import Board
+
 class SnakeAction(object):
     LEFT = 0
     RIGHT = 1
@@ -15,13 +17,21 @@ class SnakeAction(object):
 class SnakeCellState(object):
     EMPTY = 0
     WALL = 1
-    FOOD = 2
+    BITE = 2
+    FOOD = 3
 
 class SnakeReward(object):
     ALIVE = 0.
     FOOD = 1.
     DEAD = -1.
     WON = 100.
+
+class BoardColor(object):
+    BODY_COLOR = np.array([0, 0, 0], dtype=np.uint8)
+    HEAD_COLOR = np.array([255, 0, 0], dtype=np.uint8)
+    FOOD_COLOR = np.array([0, 255, 0], dtype=np.uint8)
+    SPACE_COLOR = np.array([255,255,255], dtype=np.uint8)
+
 
 class SnakeEnv(gym.Env):
 
@@ -54,6 +64,7 @@ class SnakeEnv(gym.Env):
         self.empty_cells.remove(self.snake_start)
         self.food = self.empty_cells[self.np_random.choice(len(self.empty_cells))]
         self.empty_cells.remove(self.food)
+        return self.get_image()
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -76,6 +87,9 @@ class SnakeEnv(gym.Env):
         if next_head_state == SnakeCellState.WALL:
             reward = SnakeReward.DEAD
             done = True
+        elif next_head_state == SnakeCellState.BITE:
+            reward = SnakeReward.DEAD
+            done = True
         elif next_head_state == SnakeCellState.FOOD:
             if len(self.empty_cells) > 0:
                 self.food = self.empty_cells[self.np_random.choice(len(self.empty_cells))]
@@ -83,26 +97,47 @@ class SnakeEnv(gym.Env):
                 reward = SnakeReward.FOOD
             else:
                 reward = SnakeReward.WON
+                done = True
         else:
-            self.snake.pop()
+            self.empty_cells.remove(self.snake_head)
+            emtpy_cell = self.snake.pop()
+            self.empty_cells.append(emtpy_cell)
             reward = 0.
-        return self.viewer.get_array(), reward, done, {}
+        return self.get_image(), reward, done, {}
+
+    def get_image(self):
+        board_width = 400
+        board_height = 400
+        cell_size = int(board_width / self.width)
+
+        board = Board(board_height, board_width)
+        for x, y in self.snake:
+            board.fill_cell((x*cell_size, y*cell_size), cell_size, BoardColor.BODY_COLOR)
+        x, y = self.snake[0]
+        board.fill_cell((x*cell_size, y*cell_size), cell_size, color=BoardColor.HEAD_COLOR)
+        
+        if self.food:
+            x, y = self.food
+            board.fill_cell((x*cell_size, y*cell_size), cell_size, BoardColor.FOOD_COLOR)
+        return board.board
 
     def next_head(self, action):
         x, y = self.snake_head
         if action == SnakeAction.LEFT:
-            return (x - 1, y)
+            return (x, y - 1)
         if action == SnakeAction.RIGHT:
-            return (x + 1, y)
-        if action == SnakeAction.UP:
             return (x, y + 1)
-        return (x, y - 1)
+        if action == SnakeAction.UP:
+            return (x - 1, y)
+        return (x + 1, y)
 
     def cell_state(self, cell):
         if cell in self.empty_cells:
             return SnakeCellState.EMPTY
         if cell == self.food:
             return SnakeCellState.FOOD
+        if cell in self.snake:
+            return SnakeCellState.BITE
         return SnakeCellState.WALL
 
     def is_valid_action(self, action):
@@ -117,23 +152,8 @@ class SnakeEnv(gym.Env):
         return action in horizontal_actions
 
     def render(self, mode='human'):
-        screen_width = 400
-        screen_height = 400
-        cell_size = screen_width / self.width
-
+        img = self.get_image()
         if self.viewer is None:
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-
-        for x, y in self.snake:
-            l, r, t, b = x * cell_size, (x + 1) * cell_size, y * cell_size, (y + 1) * cell_size
-            snake = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            snake.set_color(0, 0, 0)
-            self.viewer.add_onetime(snake)
-
-        if self.food:
-            x, y = self.food
-            l, r, t, b = x * cell_size, (x + 1) * cell_size, y * cell_size, (y + 1) * cell_size
-            food = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-            food.set_color(1, 0, 0)
-            self.viewer.add_onetime(food)
-        return self.viewer.render(return_rgb_array = mode == 'rgb_array')
+            self.viewer = rendering.SimpleImageViewer()
+        self.viewer.imshow(img)
+        return self.viewer.isopen
