54e8f0e5a7b55eb98732ab96e0da5690d602dfc0
xff <sailbafan@gmail.com>
2019-01-14 16:13:30 +0800

    rewrite snake_env

--- snake_gym/envs/snake_env.py
+++ snake_gym/envs/snake_env.py
@@ -1,8 +1,10 @@
 
+
 import gym
 from gym import spaces, logger
 from gym.utils import seeding
 import numpy as np
+import math
 from collections import deque
 
 
@@ -12,21 +14,11 @@ class SnakeAction(object):
     UP = 2
     DOWN = 3
 
-class SnakeCellState(object):
-    EMPTY = 0
-    WALL = 1
-    BITE = 2
-    FOOD = 3
-
-class SnakeReward(object):
-    ALIVE = 0.
-    FOOD = 1.
-    DEAD = -1.
-    WON = 100.
 
 class BoardColor(object):
     BODY_COLOR = np.array([0, 0, 0], dtype=np.uint8)
-    HEAD_COLOR = np.array([255, 0, 0], dtype=np.uint8)
+    HEAD1_COLOR = np.array([255, 0, 0], dtype=np.uint8)
+    HEAD2_COLOR = np.array([0, 0, 255], dtype=np.uint8)
     FOOD_COLOR = np.array([0, 255, 0], dtype=np.uint8)
     SPACE_COLOR = np.array([255,255,255], dtype=np.uint8)
 
@@ -39,31 +31,23 @@ class SnakeEnv(gym.Env):
     }
 
     def __init__(self):
-        self.snake = deque()
         self.width = 20
         self.hight = 20
-        self.snake_start = (10, 10)
 
         self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Box(low=0, high=255, shape=(400, 400, 3), dtype=np.uint8)
 
-        self.snake = deque()
-        self.snake_head = None
-        self.empty_cells = None
-        self.prev_action = None
+        self.snake = None
+        self.prev_act = None
         self.food = None
         self.viewer = None
         self.np_random = np.random
 
     def reset(self):
-        self.prev_action = SnakeAction.UP
-        self.snake_head = self.snake_start
-        self.empty_cells = [(x, y) for x in range(self.width) for y in range(self.hight)]
-        self.snake.clear()
-        self.snake.append(self.snake_start)
-        self.empty_cells.remove(self.snake_start)
-        self.food = self.empty_cells[self.np_random.choice(len(self.empty_cells))]
-        self.empty_cells.remove(self.food)
+        self.snake = Snake()
+        empty_cells = self.get_empty_cells()
+        empty_cells = self.snake.init(empty_cells, self.np_random)
+        self.food = empty_cells[self.np_random.choice(len(empty_cells))]
         return self.get_image()
 
     def seed(self, seed=None):
@@ -74,35 +58,30 @@ class SnakeEnv(gym.Env):
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
 
         if not self.is_valid_action(action):
-            action = self.prev_action
-        self.prev_action = action
+            action = self.prev_act
+        self.prev_act = action
 
-        next_head = self.next_head(action)
-        next_head_state = self.cell_state(next_head)
+        snake_tail = self.snake.step(action)
 
-        self.snake_head = next_head
-        self.snake.appendleft(next_head)
-
+        reward = 0.
         done = False
-        if next_head_state == SnakeCellState.WALL:
-            reward = SnakeReward.DEAD
+            
+        if self.snake.head == self.food:
+            reward += 1.
+            self.snake.snake.append(snake_tail)
+            empty_cells = self.get_empty_cells()
+            self.food = empty_cells[self.np_random.choice(len(empty_cells))]
+        
+        #snake collided wall
+        if self.is_collided_wall(self.snake.head):
+            reward -= 1.
             done = True
-        elif next_head_state == SnakeCellState.BITE:
-            reward = SnakeReward.DEAD
+        
+        #snake bite itself 
+        if self.snake.head in self.snake.body:
+            reward -= 1.
             done = True
-        elif next_head_state == SnakeCellState.FOOD:
-            if len(self.empty_cells) > 0:
-                self.food = self.empty_cells[self.np_random.choice(len(self.empty_cells))]
-                self.empty_cells.remove(self.food)
-                reward = SnakeReward.FOOD
-            else:
-                reward = SnakeReward.WON
-                done = True
-        else:
-            self.empty_cells.remove(self.snake_head)
-            emtpy_cell = self.snake.pop()
-            self.empty_cells.append(emtpy_cell)
-            reward = 0.
+
         return self.get_image(), reward, done, {}
 
     def get_image(self):
@@ -111,45 +90,42 @@ class SnakeEnv(gym.Env):
         cell_size = int(board_width / self.width)
 
         board = Board(board_height, board_width)
-        for x, y in self.snake:
+        for x, y in self.snake.body:
             board.fill_cell((x*cell_size, y*cell_size), cell_size, BoardColor.BODY_COLOR)
-        x, y = self.snake[0]
-        board.fill_cell((x*cell_size, y*cell_size), cell_size, color=BoardColor.HEAD_COLOR)
-        
+
+        x, y = self.snake.head
+        board.fill_cell((x*cell_size, y*cell_size), cell_size, color=BoardColor.HEAD1_COLOR)
+
         if self.food:
             x, y = self.food
             board.fill_cell((x*cell_size, y*cell_size), cell_size, BoardColor.FOOD_COLOR)
         return board.board
 
-    def next_head(self, action):
-        x, y = self.snake_head
-        if action == SnakeAction.LEFT:
-            return (x, y - 1)
-        if action == SnakeAction.RIGHT:
-            return (x, y + 1)
-        if action == SnakeAction.UP:
-            return (x - 1, y)
-        return (x + 1, y)
-
-    def cell_state(self, cell):
-        if cell in self.empty_cells:
-            return SnakeCellState.EMPTY
-        if cell == self.food:
-            return SnakeCellState.FOOD
-        if cell in self.snake:
-            return SnakeCellState.BITE
-        return SnakeCellState.WALL
+    def get_empty_cells(self):
+        empty_cells = [(x, y) for x in range(self.width) for y in range(self.hight)]
+        for cell in self.snake.snake:
+            if cell in empty_cells:
+                empty_cells.remove(cell)
+        if self.food in empty_cells:
+            empty_cells.remove(self.food)
+        return empty_cells
 
     def is_valid_action(self, action):
-        if len(self.snake) == 1:
+        if len(self.snake.snake) == 1:
             return True
         
         horizontal_actions = [SnakeAction.LEFT, SnakeAction.RIGHT]
         vertical_actions = [SnakeAction.UP, SnakeAction.DOWN]
 
-        if self.prev_action in horizontal_actions:
+        if self.prev_act in horizontal_actions:
             return action in vertical_actions
         return action in horizontal_actions
+
+    def is_collided_wall(self, head):
+        x, y = head
+        if x < 0 or x > 19 or y < 0 or y > 19:
+            return True
+        return False
 
     def render(self, mode='human'):
         img = self.get_image()
@@ -161,6 +137,38 @@ class SnakeEnv(gym.Env):
                 self.viewer = rendering.SimpleImageViewer()
             self.viewer.imshow(img)
             return self.viewer.isopen
+
+
+class Snake(object):
+
+    def __init__(self):
+        self.snake = deque()
+        
+    def step(self, action):
+        x, y = self.head
+        if action == SnakeAction.LEFT:
+            self.snake.appendleft((x, y - 1))
+        if action == SnakeAction.RIGHT:
+            self.snake.appendleft((x, y + 1))
+        if action == SnakeAction.UP:
+            self.snake.appendleft((x - 1, y))
+        if action == SnakeAction.DOWN:
+            self.snake.appendleft((x + 1, y))
+        return self.snake.pop()
+
+    @property
+    def head(self):
+        return self.snake[0]
+
+    @property
+    def body(self):
+        return list(self.snake)[1:]
+    
+    def init(self, empty_cells, np_random):
+        start_head = empty_cells[np_random.choice(len(empty_cells))]
+        self.snake.appendleft(start_head)
+        empty_cells.remove(start_head)
+        return empty_cells
 
 
 class Board(object):
