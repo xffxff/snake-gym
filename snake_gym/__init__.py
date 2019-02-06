72b2fdb02d86f637114977f7b9febf4693c1403e
xff <sailbafan@gmail.com>
2019-02-06 09:00:26 +0800

    register Snake-rgb-v0

--- snake_gym/__init__.py
+++ snake_gym/__init__.py
@@ -6,6 +6,12 @@ register(
 )
 
 register(
+    id='Snake-rgb-v0',
+    entry_point='snake_gym.envs:SnakeEnv',
+    kwargs={'observation_mode': 'rgb'}
+)
+
+register(
     id='MultiSnake-v0',
     entry_point='snake_gym.envs:MultiSnakeEnv',
 )
